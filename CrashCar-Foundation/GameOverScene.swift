a489dd28b51fc05864f1781f6bbffd976951ec2d
sion pardosi <spardosi12@gmail.com>
2024-06-27 15:24:21 +0700

    Initial Commit

---
+++ CrashCar-Foundation/GameOverScene.swift
@@ -0,0 +1,8 @@
+//
+//  GameOverScene.swift
+//  CrashCar-Foundation
+//
+//  Created by Foundation-024 on 28/06/24.
+//
+
+import Foundation
---
+++ CrashCar-Foundation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CrashCar-Foundation
+//
+//  Created by Foundation-024 on 27/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
